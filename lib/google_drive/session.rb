1afa9b6ce8f522166f91e507d0e73a9e577097a3
Gurbinder Gill <gill@cs.utexas.edu>
2015-02-11 16:20:39 -0600

    Update session.rb
    Try to move file to a folder directly on upload

--- lib/google_drive/session.rb
+++ lib/google_drive/session.rb
@@ -362,6 +362,10 @@ module GoogleDrive
         def upload_from_media(media, title = "Untitled", params = {})
           file = self.drive.files.insert.request_schema.new({
             "title" => title,
+            "parents": [{
+                "kind": "drive#fileLink",
+                "id": params[:folder_id]
+          }]
           })
           api_result = execute!(
               :api_method => self.drive.files.insert,
